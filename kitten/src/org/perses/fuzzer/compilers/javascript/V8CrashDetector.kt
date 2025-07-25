db4aee40b3cb0f5a54f983e49418e594ea258ba8
Srinath Srinivasan <ssrini27@ncsu.edu>
2025-07-25 17:12:51 -0400

    adding new crash detectors + config for 2 js engines

---
+++ kitten/src/org/perses/fuzzer/compilers/javascript/V8CrashDetector.kt
@@ -0,0 +1,24 @@
+package org.perses.fuzzer.compilers.javascript
+
+import org.perses.fuzzer.compilers.AbstractCompilerCrashDetector
+import org.perses.fuzzer.compilers.SanitizerCrashSignatureExtractor
+
+class V8CrashDetector : AbstractCompilerCrashDetector() {
+    override fun detectCrashSignatureFromStderr(stderr: List<String>): List<String> {
+        val result = ArrayList<String>()
+        val sanitizerCrashDetector = SanitizerCrashSignatureExtractor()
+        stderr.asSequence()
+            .filter {
+                it.isNotBlank() && it.contains(KEYWORD_FATAL_ERROR, ignoreCase = true)
+            }
+            .forEach {
+                result.add(it)
+            }
+        result.addAll(sanitizerCrashDetector.extractCrashSignatureFromStderr(stderr))
+        return result.asSequence().filter { it.isNotBlank() }.map { it.trim() }.toList()
+    }
+
+    companion object {
+        const val KEYWORD_FATAL_ERROR = "FATAL"
+    }
+} 
